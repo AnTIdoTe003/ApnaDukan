d6eb42c0832e779da6bce30de93719c6d8f25a22
Debmalya Biswas <viperbale.db@gmail.com>
2023-05-03 10:23:39 +0530

    new commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,36 +14,36 @@ export const registerUser = async(req, res)=>{
     try{
         const{name, email, password, confirmPassword, phone, address} = req.body
         if(!name){
-            res.status(400).send({success:false, message: 'Name is required'})
+            return res.status(400).send({success:false, message: 'Name is required'})
         }
         if(!password || password.length <6){
-            res.status(400).send({success:false, message: 'A stronger password must be provided'})
+            return res.status(400).send({success:false, message: 'A stronger password must be provided'})
         }
         if(password !== confirmPassword){
-            res.status(400).send({success:false, message: 'Passwords do not match'})
+            return res.status(400).send({success:false, message: 'Passwords do not match'})
         }
         if(!email){
-            res.status(400).send({success:false, message: 'Email is required'})
+            return res.status(400).send({success:false, message: 'Email is required'})
         }
         if(!phone){
-            res.status(400).send({success:false, message: 'Please enter a valid contact number'})
+            return res.status(400).send({success:false, message: 'Please enter a valid contact number'})
         }
         if(!address){
-            res.status(400).send({success:false, message: 'Address na diley kaar baritey pathabo'})
+            return res.status(400).send({success:false, message: 'Address na diley kaar baritey pathabo'})
         }
         const existEmail = await userModel.findOne({email: email})
         if(existEmail){
-            res.status(400).send({success:false, message:'Email already exists'})
+            return res.status(400).send({success:false, message:'Email already exists'})
         }
         const hashedPassword = await hashPassword(password)
         const confirmHashedPassword = await hashPassword(confirmPassword)
         const userInfo = await userModel.create({...req.body, password:hashedPassword, confirmPassword:confirmHashedPassword})
-        return res.status(200).send({success:true, message:"Successfully created the account", userInfo})
+        return res.setHeader('Content-Type', 'application/json').status(200).send({success:true, message:"Successfully created the account", userInfo})
     }catch(error){
-        res.status(404).send({
+        return res.status(404).send({
             success:false,
             message:"Error registering user",
-            error:error
+            error:errorreturn 
         })
         console.log(error)
     }
@@ -55,29 +55,29 @@ export const loginUser = async(req,res)=>{
     try{
         const {email, password} = req.body
         if(!email || !password){
-            res.status(401).send({success:false, message:"Invalid email or password"})
+            return res.status(401).send({success:false, message:"Invalid email or password"})
         }
         const existUser = await userModel.findOne({email:email})
         if(!existUser){
-            res.send(401).status({success:false, message:"User does not exist"})
+            return res.status(401).send({success:false, message:"User does not exist"})
         }
         const matchPassword = await comparePassword(password, existUser.password)
         if(!matchPassword){
-            res.status(401).send({success:false, message:"Please enter a valid password"})
+            return res.status(401).send({success:false, message:"Please enter a valid password"})
         }
 
         // token
         const token = await JWT.sign({_id:existUser._id}, process.env.JWT_SECRET,{expiresIn:"7d"})
-        res.status(200).send({
+        return res.setHeader('Content-Type', 'application/json').status(200).send({
             success:true,
             message:"User Logged in successfully",
             existUser,
             token
         })
     }catch(error){
-        res.status(404).send({
+        return res.status(404).send({
             success:false,
-            message:"Error loggin in user",
+            message:"Error logging in user",
             error:error
         })
         console.log(error)
--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ const app = express()
 connectDb()
 
 // middleware
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:3000'
+}))
 app.use(express.json())
 app.use(morgan('dev'))
 
